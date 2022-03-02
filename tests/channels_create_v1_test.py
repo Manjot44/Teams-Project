72b61b99f7126f39ec57180a8d679a0aef0cdd60
Ashwin <z5363368@ad.unsw.edu.au>
2022-03-02 16:55:48 +1100

    Edited test file, changed location

---
+++ tests/channels_create_v1_test.py
@@ -0,0 +1,16 @@
+import sys
+import pytest
+from src.channels import channels_create_v1
+from src.error import InputError
+
+def test_normal():
+    assert type(channels_create_v1(1234567, "new_channel", True)["channel_id"]) == int
+
+def test_border():
+    assert type(channels_create_v1(1234567, "a", True)["channel_id"]) == int
+    assert type(channels_create_v1(1234567, "abcdefghijklmnopqrstuvwxyz", True)["channel_id"]) == int
+
+def test_edge():
+    with pytest.raises(InputError):
+        channels_create_v1(1234567, "", True)["channel_id"]
+        channels_create_v1(1234567, "abcdefghijklmnopqrstuvwxyz0", True)["channel_id"]
