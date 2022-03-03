d50e1d4d262d8f2e839f22be47a372950446f864
Ashwin <z5363368@ad.unsw.edu.au>
2022-03-03 11:20:20 +1100

    Finished implemenation, made slight changes to testing file

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,10 @@
+if __name__ == '__main__':
+    from error import InputError
+    from data_store import data_store
+else:
+    from src.error import InputError
+    from src.data_store import data_store
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +26,56 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
-    return {
-        'channel_id': 1,
-    }
+    #Assessing for exception
+    namelen = len(name)
+    if namelen < 1 or namelen > 20:
+        raise InputError(f"Name must be between 1 and 20 characters long")
+    
+    #Assigning variable to check if this is the first channel created
+    initialchannel = False
+
+    #Getting Data from data storage file
+    store = data_store.get()
+
+    #Creates list of all channels if it does not already exist
+    if "channels" not in store:
+        nc = {      
+        }
+        nc["channel_id"] = 1
+        store["channels"] = [nc]
+        initialchannel = True
+    
+    #Creating new channel ID and adding to the channels list
+    channel_id = 1
+    i = 0
+    while True:
+        if initialchannel == True:
+            break
+        if i == len(store["channels"]):
+            nnc = {
+            }
+            nnc["channel_id"] = channel_id
+            store["channels"].append(nnc)
+            break
+        channel_id += 1
+        i += 1
+    
+    #Assigning variable to newly created channel
+    current_channel = store["channels"][i]
+
+    #Assigning user inputs
+    current_channel["channel_id"] = channel_id
+    current_channel["members"] = [auth_user_id]
+    current_channel["name"] = name
+    current_channel["ispublic"] = is_public
+
+    #Saving to datastore
+    data_store.set(store)
+
+    #Testing
+    print(current_channel)
+    return channel_id
+
+
+if __name__ == '__main__':
+    channels_create_v1("abcdefgh", "test-channel-1", False)
--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -4,13 +4,13 @@ from src.channels import channels_create_v1
 from src.error import InputError
 
 def test_normal():
-    assert type(channels_create_v1(1234567, "new_channel", True)["channel_id"]) == int
+    assert type(channels_create_v1(1234567, "new_channel", True)) == int
 
 def test_border():
-    assert type(channels_create_v1(1234567, "a", True)["channel_id"]) == int
-    assert type(channels_create_v1(1234567, "abcdefghijklmnopqrst", True)["channel_id"]) == int
+    assert type(channels_create_v1(1234567, "a", True)) == int
+    assert type(channels_create_v1(1234567, "abcdefghijklmnopqrst", True)) == int
 
 def test_edge():
     with pytest.raises(InputError):
-        channels_create_v1(1234567, "", True)["channel_id"]
-        channels_create_v1(1234567, "abcdefghijklmnopqrst0", True)["channel_id"]
+        channels_create_v1(1234567, "", True)
+        channels_create_v1(1234567, "abcdefghijklmnopqrst0", True)
