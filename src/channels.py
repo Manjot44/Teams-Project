7eb833bc3427c899ad9cc1146a0482fcedfc6d93
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-03-04 00:10:33 +1100

    First iteration of channels_listall_v1(); seemingly works but may need additional work to be ready for merging.

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from data_store import data_store
+
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -9,16 +12,41 @@ def channels_list_v1(auth_user_id):
     }
 
 def channels_listall_v1(auth_user_id):
-    return {
+    listall = {
+        'channels': []
+    }
+    store = data_store.get()
+    stored_channel_list = store['channels']
+    for channel in stored_channel_list:
+        listall['channels'].append(channel)
+    
+    return listall
+
+    '''
+    return 
+    {
         'channels': [
         	{
         		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
+        		'name': 'My Channel 1',
+                'members' = [],
+                'ispublic' = True,
+        	},
+            {
+                'channel_id': 2,
+        		'name': 'My Channel 2',
+            },
         ],
     }
+    '''
+
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
         'channel_id': 1,
     }
+
+if __name__ == '__main__':
+    
+    list_all = channels_listall_v1(0)
+    print(list_all)
