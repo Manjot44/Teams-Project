7dda6187f32f83b01684a61c8452308e9315d043
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-03-03 21:23:54 +1100

    Major tests written including unit tests for invalid auth user/channel ids

---
+++ tests/details_test.py
@@ -0,0 +1,57 @@
+import pytest
+from src import auth, channel, channels, error, other
+
+def test_details_invalid_auth_user_id():
+    other.clear_v1()
+    valid_user_id = auth.auth_register_v1('sanjamsingh@gmail.com', 'password', 'sanjam', 'singh')
+    invalid_user_id = None
+    valid_channel_id = channels.channels_create_v1(valid_user_id, 'sanjam_channel', True)
+    with pytest.raises(error.AccessError):
+        # tests for invalid user id for a channel id
+        assert channel.channel_details_v1(invalid_user_id, valid_channel_id)
+
+def test_details_invalid_channel_id():
+    other.clear_v1()
+    user_id = auth.auth_register_v1('sanjamsingh@gmail.com', 'password', 'sanjam', 'singh')
+    handle = 'sanjamsingh'
+    with pytest.raises(error.InputError):
+        assert channel.channel_details_v1(user_id, None)                                            # channel id is non existant
+        assert channel.channel_details_v1(user_id, 'random_string')                                 # channel id is not an integer
+
+def test_details_invalid_channel_access():
+    other.clear_v1()
+    user_id1 = auth.auth_register_v1('sanjamsingh@gmail.com', 'password', 'sanjam', 'singh')        # user id 1
+    user_id2 = auth.auth_register_v1('jerrylin@gmail.com', 'password', 'jerry', 'lin')              # user id 2
+    channel_id1 = channels.channels_create_v1(user_id1, 'sanjam_channel', True)                     # create channel 1
+    channel_id2 = channels.channels_create_v1(user_id2, 'jerry_channel', True)                      # create channel 2
+    with pytest.raises(error.AccessError):                                                                # details(user 1, channel 2)
+        assert channel.channel_details_v1(user_id1, channel_id2)                                    # raise access error 
+        assert channel.channel_details_v1(user_id2, channel_id1)                                    # raise access error
+
+def test_details_user_of_created_channel():
+    other.clear_v1()
+    user_id = auth.auth_register_v1('sanjamsingh@gmail.com', 'password', 'sanjam', 'singh')
+    handle = 'sanjamsingh'
+    channel_id = channels.channels_create_v1(user_id, 'sanjam_channel', True)
+    details = channel.channel_details_v1(user_id, channel_id)
+    assert details['name'] == 'sanjam'
+
+    for members in details['owner members']:
+        assert members['owner_members']['u_id'] == user_id
+        assert members['owner_members']['email'] == 'sanjamsingh@gmail.com'
+        assert members['owner_members']['name_first'] == 'sanjam'
+        assert members['owner_members']['name_last'] == 'singh'
+        assert members['owner_members']['handle_str'] == handle
+
+    for members in details['owner members']:
+        assert members['owner_members']['u_id'] == user_id
+        assert members['owner_members']['email'] == 'sanjamsingh@gmail.com'
+        assert members['owner_members']['name_first'] == 'sanjam'
+        assert members['owner_members']['name_last'] == 'singh'
+        assert members['owner_members']['handle_str'] == handle
+
+
+'''
+Test for:
+
+'''
