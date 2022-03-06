209b0906d386ad239e197417599fce1865a619eb
Manjot <z5359423@ad.unsw.edu.au>
2022-03-06 19:00:51 +1100

    adding handle tests

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,4 +1,7 @@
+from turtle import clear
 import src.auth
+import src.channels
+import src.channel
 import pytest 
 from src.other import clear_v1
 from src.error import InputError
@@ -147,3 +150,52 @@ def test_authlog_none():
     clear_v1()
     with pytest.raises(InputError):
         src.auth.auth_login_v1("anything@gmail.com", "password1$^")                      # no accounts stored
+
+
+
+def test_handle_normal():
+    clear_v1()
+    user_id = src.auth.auth_register_v1("jerrylin@gmail.com", "123456", "Jerry", "Lin")["auth_user_id"]
+    channel_id = src.channels.channels_create_v1(user_id, "new channel", True)["channel_id"]
+    generated_handle = src.channel.channel_details_v1(user_id, channel_id)["all_members"][0]["handle_str"]
+
+    expected_handle = "jerrylin"
+    assert generated_handle == expected_handle
+
+def test_handle_long1():
+    clear_v1()
+    user_id = src.auth.auth_register_v1("jerrylin@gmail.com", "123456", "Iamthegreatest", "JerryL")["auth_user_id"]
+    channel_id = src.channels.channels_create_v1(user_id, "new channel", True)["channel_id"]
+    generated_handle = src.channel.channel_details_v1(user_id, channel_id)["all_members"][0]["handle_str"]
+
+    expected_handle = "iamthegreatestjerryl"
+    assert generated_handle == expected_handle
+
+def test_handle_long2():
+    clear_v1()
+    user_id = src.auth.auth_register_v1("jerrylin@gmail.com", "123456", "Iamthegreatest", "JerryLi")["auth_user_id"]
+    channel_id = src.channels.channels_create_v1(user_id, "new channel", True)["channel_id"]
+    generated_handle = src.channel.channel_details_v1(user_id, channel_id)["all_members"][0]["handle_str"]
+
+    expected_handle = "iamthegreatestjerryl"
+    assert generated_handle == expected_handle
+
+def test_handle_repeat1():
+    clear_v1()
+    src.auth.auth_register_v1("jerrylin@gmail.com", "123456", "Jerry", "Lin")
+    user_id = src.auth.auth_register_v1("jerry@gmail.com", "123456", "Jerry", "Lin")["auth_user_id"]
+    channel_id = src.channels.channels_create_v1(user_id, "new channel", True)["channel_id"]
+    generated_handle = src.channel.channel_details_v1(user_id, channel_id)["all_members"][0]["handle_str"]
+
+    expected_handle = "jerrylin0"
+    assert generated_handle == expected_handle
+
+def test_handle_repeat2():
+    clear_v1()
+    src.auth.auth_register_v1("jerrylin@gmail.com", "123456", "Iamthegreatest", "JerryLi")
+    user_id = src.auth.auth_register_v1("jerry@gmail.com", "123456", "Iamthegreatest", "JerryLi")["auth_user_id"]
+    channel_id = src.channels.channels_create_v1(user_id, "new channel", True)["channel_id"]
+    generated_handle = src.channel.channel_details_v1(user_id, channel_id)["all_members"][0]["handle_str"]
+
+    expected_handle = "iamthegreatestjerryl0"
+    assert generated_handle == expected_handle
