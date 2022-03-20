f88d03e1eca4bbf66dcf29b72646025585725f6e
Manjot <z5359423@ad.unsw.edu.au>
2022-03-20 18:13:07 +1100

    adding data.set() at the end of the implementation function

--- src/channel_expansion.py
+++ src/channel_expansion.py
@@ -19,6 +19,7 @@ def channel_leave_v1(token, channel_id):
         (dict): returns an empty dictionary
     '''
     store = data_store.get()
+    
     auth_user_id = src.error_help.check_valid_token(token, store)
     src.error_help.validate_channel(store, channel_id)
     src.error_help.user_not_in_channel(store, auth_user_id, channel_id)    
@@ -33,5 +34,6 @@ def channel_leave_v1(token, channel_id):
             store["channels"][channel_id]["owner_members"].pop(idx)
             break
     
+    data_store.set(store)
     return {
     }
