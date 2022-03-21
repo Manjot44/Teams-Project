6fc24f7fa4e99c77f62bb96b7cd8b89bd30bc244
Manjot <z5359423@ad.unsw.edu.au>
2022-03-21 14:06:04 +1100

    fixed duplicate test

--- tests/channel_addowner_test.py
+++ tests/channel_addowner_test.py
@@ -49,6 +49,9 @@ def test_invalid_uid(register_three_users):
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
+    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token:": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    assert response.status_code == 200
+
     response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": None})
     assert response.status_code == 400
 
