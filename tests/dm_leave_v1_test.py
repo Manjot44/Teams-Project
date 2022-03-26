c1e24bff08afdd716f6aa850b0d610146a4ee5b6
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-03-27 01:49:05 +1100

    Wrote tests for dm leave which test all the exceptions mentioned in the spec

---
+++ tests/dm_leave_v1_test.py
@@ -0,0 +1,38 @@
+import requests
+
+BASE_ADDRESS = 'http://127.0.0.1'
+BASE_PORT = 8080
+BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
+
+def test_dm_leave_input_error_invalid_dm_id(register_three_users):
+    token = register_three_users['token'][0]
+    dm_id = None
+    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': token, 'dm_id': dm_id})
+    assert response.status_code == 400
+
+def test_dm_leave_invalid_token_valid_dm_id(register_three_users):
+    invalid_token = None
+    valid_token = register_three_users['token'][0]
+    member_id = register_three_users['id'][1]
+    #run dm create with one user to another user
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={'token': valid_token, 'u_ids': [member_id]})
+    assert response == 200
+    dm_id = response.json()
+    #run dm leave with wrong token and dm_id
+    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': invalid_token, 'dm_id': dm_id['dm_id']})
+    assert response.status_code == 403
+
+def test_dm_leave_access_error_valid_dm_id_user_not_member(register_three_users):
+    nonmember_token = register_three_users['token'][2]
+    member_token = register_three_users['token'][0]
+    member_id = register_three_users['id'][1]
+    #run dm create with one user to another user
+    response = requests.post(f"{BASE_URL}/dm/create/v1", json={'token': member_token, 'u_ids': [member_id]})
+    assert response == 200
+    dm_id = response.json()
+    #run dm leave with wrong token and dm_id
+    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': nonmember_token, 'dm_id': dm_id})
+    assert response.status_code == 403
+
+def test_kdsjhfksdj():
+    pass
