eb907401637bf5faf55767653a7161ca1055948f
Sanjam <z5363367@ad.unsw.edu.au>
2022-03-28 06:02:29 +1100

    added tests to user_profile and edites admin func

--- src/admin.py
+++ src/admin.py
@@ -30,7 +30,6 @@ def admin_userpermission_change(token, u_id, permission_id):
     check_valid_id(u_id, store)
     check_valid_id(auth_user, store)
 
-    # Check for Errors
     if permission_id < 1 or permission_id > 2:
         raise InputError(f"Error: {permission_id} invalid permission id, permission id must either be 1 or 2.")
     if permission_id == store['users'][u_id]['perm_id']:
@@ -38,17 +37,16 @@ def admin_userpermission_change(token, u_id, permission_id):
     if store['users'][auth_user]['perm_id'] == 2:
         raise AccessError(f"Error: {auth_user} is not a global owner")
     
-    # Change the perm_id of u_id
+
     if permission_id == 1 and store['users'][u_id]['perm_id'] == 2:
         store['users'][u_id]['perm_id'] = permission_id
     if permission_id == 2 and store['users'][u_id]['perm_id'] == 1:
         
-        # Count how many global owners there are
         global_count = 0
         for user in store['users']:
             if user['perm_id'] == 1:
                 global_count += 1
-        # Ensure that there is at least 1 global owner in Seams
+        
         if global_count > 1:
             store['users'][u_id]['perm_id'] = permission_id
         else:
@@ -58,14 +56,6 @@ def admin_userpermission_change(token, u_id, permission_id):
     return {
     }
 
-# Prevents a 'removed user' from being added to the removed_user list twice
-def check_removed_user_in_list(u_id, data_store):
-    has_removed_user = False
-    for removed_user in data_store["removed_users"]:
-        if removed_user["u_id"] == u_id:
-            has_removed_user = True
-
-    return has_removed_user
 
 def admin_user_remove(token, u_id):
     ''' Given a user by their u_id, remove them from the Seams. 
@@ -92,16 +82,13 @@ def admin_user_remove(token, u_id):
     '''
     store = data_store.get()
 
-    # Check if auth_user_id and u_id are valid
     auth_user = check_valid_token(token, store)
     check_valid_id(u_id, store)
     check_valid_id(auth_user, store)
 
-    # AccessError if auth_user is not a global owner
     if store['users'][auth_user]['perm_id'] == 2:
         raise AccessError(f"Error: {auth_user} is not a global owner")
 
-    # Ensures that there is at least one global owner
     global_count = 0
     if store['users'][auth_user]['perm_id'] == 1:
         for user in store['users']:
@@ -109,85 +96,44 @@ def admin_user_remove(token, u_id):
                 global_count += 1
     if global_count == 1:
         raise InputError(f"Error: Need to at least have one one global owner")
+    store['removed_users'] = []
+    add_removed_user = {
+        'u_id': u_id,
+        'email': None,
+        'name_first': 'Removed',
+        'name_last': 'user',
+        'handle_str': None,
+    }
+    store["removed_users"].append(add_removed_user)
+    for user in store['users']:
+        if user['u_id'] == u_id:
+            del user
 
-    # Remove from channel
     for channel in store["channels"]:
         for user in channel["all_members"]:
             if user["u_id"] == u_id:
                 del user
 
-                add_removed_user = {
-                    'u_id': u_id,
-                    'email': None,
-                    'name_first': 'Removed',
-                    'name_last': 'user',
-                    'handle_str': None,
-                }
-                
-                has_removed_user = check_removed_user_in_list(u_id, store)
-                if has_removed_user == False:
-                    store["removed_users"].append(add_removed_user)
-        
-        for user1 in channel["owner_members"]: # meant to remove from both the all_members and owner_members list 
+        for user1 in channel["owner_members"]:
             if user1["u_id"] == u_id:
                 del user1
                 
-                add_removed_user = {
-                    'u_id': u_id,
-                    'email': None,
-                    'name_first': 'Removed',
-                    'name_last': 'user',
-                    'handle_str': None,
-                }
-                
-                has_removed_user = check_removed_user_in_list(u_id, store)
-                if has_removed_user == False:
-                    store["removed_users"].append(add_removed_user)
-            
-    # Remove from DM
     for dm in store["dms"]:
         for user_dm in dm["all_members"]:
             if user_dm["u_id"] == u_id:
                 del user_dm 
-
-                add_removed_user = {
-                    'u_id': u_id,
-                    'email': None,
-                    'name_first': 'Removed',
-                    'name_last': 'user',
-                    'handle_str': None,
-                }
-                
-                has_removed_user = check_removed_user_in_list(u_id, store)
-                if has_removed_user == False:
-                    store["removed_users"].append(add_removed_user)
         
         for user_dm1 in dm["owner_members"]:
             if user_dm1["u_id"] == u_id:
                 del user_dm1
-        
-                add_removed_user = {
-                    'u_id': u_id,
-                    'email': None,
-                    'name_first': 'Removed',
-                    'name_last': 'user',
-                    'handle_str': None,
-                }
-                
-                has_removed_user = check_removed_user_in_list(u_id, store)
-                if has_removed_user == False:
-                    store["removed_users"].append(add_removed_user)
 
-        # Replace all dms sent with "Replace user"
         for dm_message in dm["messages"]:
             if dm_message["u_id"] == u_id:
                 dm_message["message"] = 'Removed user'
 
-    # Remove Channel messages 
     for msg in store["messages"]:
         if msg["u_id"] == u_id:
             msg["message"] = 'Removed user'
-
     data_store.set(store)
     return {
     }
--- tests/user_profile_v1_http_test.py
+++ tests/user_profile_v1_http_test.py
@@ -24,7 +24,7 @@ def test_invalid_token():
     response2 = requests.get(f"{BASE_URL}/user/profile/v1", params = {"token": -1, "u_id": user['auth_user_id']})
     assert response2.status_code == 403
 
-def test_correct_return(register_three_users):
+def test_correct_return():
     requests.delete(f"{BASE_URL}/clear/v1")
 
     response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
@@ -39,4 +39,29 @@ def test_correct_return(register_three_users):
                                     'name_first': 'Jerry', 
                                     'name_last': 'Lin', 
                                     'handle_str': 'jerrylin'
+    }}
+
+def test_correct_return_removed_user(): 
+    requests.delete(f"{BASE_URL}/clear/v1")
+
+    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    user = response.json()
+    
+    response1 = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "manjot@gmail.com", "password": "ManYlin4", "name_first": "Manjot", "name_last": "Bhathal"})
+    user2 = response1.json()
+
+    requests.post(f"{BASE_URL}/admin/userpermission/change/v1", json = {"token": user['token'], "u_id": user2['auth_user_id'], "permission_id": 1})
+
+    response2 = requests.delete(f"{BASE_URL}/admin/user/remove/v1", json = {"token": user['token'], "u_id": user['auth_user_id']})
+    assert response2.status_code == 200
+
+    response3 = requests.get(f"{BASE_URL}/user/profile/v1", params = {"token": user2['token'], "u_id": user['auth_user_id']}) 
+    assert response3.status_code == 200
+
+    assert response3.json() == {'user': {
+                                    'u_id': user['auth_user_id'], 
+                                    'email': None, 
+                                    'name_first': 'Removed', 
+                                    'name_last': 'user', 
+                                    'handle_str': None
     }}
