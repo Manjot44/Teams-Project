0c37270edf74087a89c1e5b276bd976afefbbc1e
Sanjam <z5363367@ad.unsw.edu.au>
2022-03-27 22:09:48 +1100

    made changes to user_profile implementation

--- src/user.py
+++ src/user.py
@@ -9,36 +9,52 @@ import re
 def user_profile_v1(token, u_id):
     store = data_store.get()
     
-    valid_u_id = 0
+    valid_u_id = "invalid"
+    
     u_id_check = store['users']
     for user in u_id_check:
         if user['u_id'] == u_id:
-            valid_u_id = 1
+            valid_u_id = "valid_user"
+    
+    removed_u_id_check = store['removed_users']
+    for user in removed_u_id_check:
+        if user['u_id'] == u_id:
+            valid_u_id = "valid_removed_user"
+    if valid_u_id == "invalid":
+        raise InputError(f"u_id does not refer to a valid user")
 
-    if valid_u_id == 0:
-        raise InputError
-
-    u_id = check_valid_token(token, store)
+    check_valid_token(token, store)
           
     user_info_dict = {}
     user_info_dict['user'] = {}
     user_info = store['users']
-    for user in user_info:
-        if user['u_id'] == u_id:  
-            user_info_dict['user']['u_id'] = user['u_id']
-            user_info_dict['user']['email'] = user['email']
-            user_info_dict['user']['name_first'] = user['name_first']
-            user_info_dict['user']['name_last'] = user['name_last']
-            user_info_dict['user']['handle_str'] = user['handle_str']
-        return user_info_dict
+    removed_user_info = store['removed_users']
+    if valid_u_id == "valid_user":
+        for user in user_info:
+            if user['u_id'] == u_id:  
+                user_info_dict['user']['u_id'] = user['u_id']
+                user_info_dict['user']['email'] = user['email']
+                user_info_dict['user']['name_first'] = user['name_first']
+                user_info_dict['user']['name_last'] = user['name_last']
+                user_info_dict['user']['handle_str'] = user['handle_str']
+            return user_info_dict
+    elif valid_u_id == "valid_removed_user":
+        for user in removed_user_info:
+            if user['u_id'] == u_id:  
+                user_info_dict['user']['u_id'] = user['u_id']
+                user_info_dict['user']['email'] = user['email']
+                user_info_dict['user']['name_first'] = user['name_first']
+                user_info_dict['user']['name_last'] = user['name_last']
+                user_info_dict['user']['handle_str'] = user['handle_str']
+            return user_info_dict
 
 def user_profile_setname_v1(token, name_first, name_last):
     store = data_store.get()
 
     if len(name_first) > 50 or len(name_first) < 1:
-            raise InputError
+            raise InputError(f"length of name_first is not between 1 and 50 characters inclusive")
     if len(name_last) > 50 or len(name_last) < 1:
-        raise InputError
+        raise InputError(f"length of name_last is not between 1 and 50 characters inclusive")
     
     u_id = check_valid_token(token, store)
      
@@ -55,14 +71,14 @@ def user_profile_setemail_v1(token, email):
     user_info = store['users']
     for user in user_info:
         if user['email'] == email:
-            raise InputError 
+            raise InputError(f"email address is already being used by another user")
 
     valid_email = True
     regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
     if not re.match(regex, email):
         valid_email = False
     if valid_email == False:
-        raise InputError
+        raise InputError(f"email entered is not a valid email ")
 
     u_id = check_valid_token(token, store)
     
@@ -76,15 +92,15 @@ def user_profile_sethandle_v1(token, handle_str):
     store = data_store.get()
 
     if len(handle_str) > 20 or len(handle_str) < 3:
-        raise InputError
+        raise InputError(f"length of handle_str is not between 3 and 20 characters inclusive")
     
     if not handle_str.isalnum():
-        raise InputError
+        raise InputError(f"handle_str contains characters that are not alphanumeric")
 
     user_info = store['users']
     for user in user_info:
         if user['handle_str'] == handle_str:
-            raise InputError 
+            raise InputError(f"the handle is already used by another user")
     
     u_id = check_valid_token(token, store) 
 
