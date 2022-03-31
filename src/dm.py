23da97ff7e01e3303b13432e19bc8204b603a687
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-03-31 11:42:47 +1100

    Bug fix for iteration 3: dm_messages now returns list of messages instead of nested list of list of messages.

--- src/dm.py
+++ src/dm.py
@@ -362,9 +362,9 @@ def dm_messages_v1(token, dm_id, start):
     last_idx = len(total_message_list)
     if len(total_message_list[start:last_idx]) <= 50:
         messages['end'] = -1
-        messages['messages'].append(total_message_list[start:last_idx])
+        messages['messages'] = total_message_list[start:last_idx]
     else:
-        messages['messages'].append(total_message_list[start:start + 50])
+        messages['messages'] = total_message_list[start:start + 50]
 
     return messages
     
