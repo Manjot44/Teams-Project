91160dc6bfa0911258c6fdd4c8bfe178471a14b1
Manjot <z5359423@ad.unsw.edu.au>
2022-04-02 22:41:01 +1100

    added config for all manjots tests

--- tests/channel_addowner_test.py
+++ tests/channel_addowner_test.py
@@ -1,23 +1,20 @@
 import pytest
 import requests
-
-BASE_ADDRESS = 'http://127.0.0.1'
-BASE_PORT = 8080
-BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
+import src.config
 
 def test_valid_add(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][1], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][1], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "u_id": register_three_users["id"][0]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "u_id": register_three_users["id"][0]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][0]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][0]})
     assert response.status_code == 200
 
-    response = requests.get(f"{BASE_URL}/channel/details/v2?token={register_three_users['token'][0]}&channel_id={channel_id}")
+    response = requests.get(f"{src.config.url}/channel/details/v2?token={register_three_users['token'][0]}&channel_id={channel_id}")
     assert response.status_code == 200
     response_data = response.json()
     owner_members = response_data["owner_members"]
@@ -29,80 +26,80 @@ def test_valid_add(register_three_users):
     assert is_owner
 
 def test_invalid_channel(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": None, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": None, "u_id": register_three_users["id"][1]})
     assert response.status_code == 400
 
 def test_invalid_uid(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": None})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": None})
     assert response.status_code == 400
 
 def test_invalid_member(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 400
 
 def test_already_owner(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 400
 
 def test_invalid_permissions(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][2]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][2]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "u_id": register_three_users["id"][2]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "u_id": register_three_users["id"][2]})
     assert response.status_code == 403
 
 def test_invalid_token_registered(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/invite/v2", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": None, "channel_id": channel_id, "u_id": register_three_users["id"][1]})
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": None, "channel_id": channel_id, "u_id": register_three_users["id"][1]})
     assert response.status_code == 403
 
 def test_invalid_token_unregistered():
-    requests.delete(f"{BASE_URL}/clear/v1")
-    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = {"token": None, "channel_id": 0, "u_id": 0})
+    requests.delete(f"{src.config.url}/clear/v1")
+    response = requests.post(f"{src.config.url}/channel/addowner/v1", json = {"token": None, "channel_id": 0, "u_id": 0})
     assert response.status_code == 403
--- tests/message_send_test.py
+++ tests/message_send_test.py
@@ -1,45 +1,42 @@
 import pytest
 import requests
-
-BASE_ADDRESS = 'http://127.0.0.1'
-BASE_PORT = 8080
-BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
+import src.config
 
 def test_valid_message(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "message": "Hello Sanjam"})
+    response = requests.post(f"{src.config.url}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "message": "Hello Sanjam"})
     assert response.status_code == 200
 
 def test_invalid_channel(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
 
-    response = requests.post(f"{BASE_URL}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": "landejend", "message": "Hello Sanjam"})
+    response = requests.post(f"{src.config.url}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": "landejend", "message": "Hello Sanjam"})
     assert response.status_code == 400
 
 def test_bad_message(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "message": None})
+    response = requests.post(f"{src.config.url}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "message": None})
     assert response.status_code == 400
 
 def test_not_user(register_three_users):
-    response = requests.post(f"{BASE_URL}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    response = requests.post(f"{src.config.url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
     response_data = response.json()
     channel_id = response_data["channel_id"]
 
-    response = requests.post(f"{BASE_URL}/message/send/v1", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "message": "Hello Sanjam"})
+    response = requests.post(f"{src.config.url}/message/send/v1", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "message": "Hello Sanjam"})
     assert response.status_code == 403
 
 def test_invalid_token():
-    requests.delete(f"{BASE_URL}/clear/v1")
-    response = requests.post(f"{BASE_URL}/message/send/v1", json = {"token": None, "channel_id": 0, "message": "Hello Sanjam"})
+    requests.delete(f"{src.config.url}/clear/v1")
+    response = requests.post(f"{src.config.url}/message/send/v1", json = {"token": None, "channel_id": 0, "message": "Hello Sanjam"})
     assert response.status_code == 403
