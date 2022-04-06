40c17a8a6ca0b6f0420a72ba4f3dc90e6dc69dec
Jerry Lin <z5363361@ad.unsw.edu.au>
2022-04-06 20:25:51 +1000

    fixed config url

--- tests/dm_leave_v1_test.py
+++ tests/dm_leave_v1_test.py
@@ -1,13 +1,10 @@
 import requests
-
-BASE_ADDRESS = 'http://127.0.0.1'
-BASE_PORT = 8080
-BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
+from src.config import url
 
 def test_dm_leave_input_error_valid_token_invalid_dm_id(register_three_users):
     token = register_three_users['token'][0]
     dm_id = None
-    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': token, 'dm_id': dm_id})
+    response = requests.post(f"{url}/dm/leave/v1", json={'token': token, 'dm_id': dm_id})
     assert response.status_code == 400
 
 def test_dm_leave_invalid_token_valid_dm_id(register_three_users):
@@ -15,11 +12,11 @@ def test_dm_leave_invalid_token_valid_dm_id(register_three_users):
     valid_token = register_three_users['token'][0]
     member_id = register_three_users['id'][1]
     #run dm create with one user to another user
-    response = requests.post(f"{BASE_URL}/dm/create/v1", json={'token': valid_token, 'u_ids': [member_id]})
+    response = requests.post(f"{url}/dm/create/v1", json={'token': valid_token, 'u_ids': [member_id]})
     assert response.status_code == 200
     dm_id = response.json()
     #run dm leave with wrong token and dm_id
-    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': invalid_token, 'dm_id': dm_id['dm_id']})
+    response = requests.post(f"{url}/dm/leave/v1", json={'token': invalid_token, 'dm_id': dm_id['dm_id']})
     assert response.status_code == 403
 
 def test_dm_leave_access_error_valid_dm_id_user_not_member(register_three_users):
@@ -27,25 +24,25 @@ def test_dm_leave_access_error_valid_dm_id_user_not_member(register_three_users)
     member_token = register_three_users['token'][0]
     member_id = register_three_users['id'][1]
     #run dm create with one user to another user
-    response = requests.post(f"{BASE_URL}/dm/create/v1", json={'token': member_token, 'u_ids': [member_id]})
+    response = requests.post(f"{url}/dm/create/v1", json={'token': member_token, 'u_ids': [member_id]})
     assert response.status_code == 200
     dm_id = response.json()
     dm_id = dm_id['dm_id']
     #run dm leave with wrong token and dm_id
-    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': nonmember_token, 'dm_id': dm_id})
+    response = requests.post(f"{url}/dm/leave/v1", json={'token': nonmember_token, 'dm_id': dm_id})
     assert response.status_code == 403
 
 def test_dm_leave_valid_input_creator_leaving(register_three_users):
     leaver_token = register_three_users['token'][0]
     member_id = register_three_users['id'][1]
-    response = requests.post(f"{BASE_URL}/dm/create/v1", json={'token': leaver_token, 'u_ids': [member_id]})
+    response = requests.post(f"{url}/dm/create/v1", json={'token': leaver_token, 'u_ids': [member_id]})
     assert response.status_code == 200
     dm_id = response.json()
     dm_id = dm_id['dm_id']
-    response = requests.post(f"{BASE_URL}/dm/leave/v1", json={'token': leaver_token, 'dm_id': dm_id})
+    response = requests.post(f"{url}/dm/leave/v1", json={'token': leaver_token, 'dm_id': dm_id})
     assert response.status_code == 200
     member_token = register_three_users['token'][1]
-    response = requests.get(f"{BASE_URL}/dm/details/v1?token={member_token}&dm_id={dm_id}")
+    response = requests.get(f"{url}/dm/details/v1?token={member_token}&dm_id={dm_id}")
     assert response.status_code == 200
     details = response.json()
     assert len(details['members']) == 1
