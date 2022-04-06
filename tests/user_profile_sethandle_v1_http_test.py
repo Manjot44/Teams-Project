0ba6c3c0f1f90cfe772bc3534b0d3808d48a77f5
Sanjam <z5363367@ad.unsw.edu.au>
2022-04-06 14:56:32 +1000

    src.config

--- tests/user_profile_sethandle_v1_http_test.py
+++ tests/user_profile_sethandle_v1_http_test.py
@@ -1,20 +1,18 @@
 import requests
 import pytest
+import src.config
 
-BASE_ADDRESS = 'http://127.0.0.1'
-BASE_PORT = 8080
-BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
 
 def test_correct_output():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
     
-    response2 =  requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathal"})  
+    response2 =  requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathal"})  
     assert response2.status_code == 200
 
-    response3 = requests.get(f"{BASE_URL}/user/profile/v1", params = {"token": user1["token"], "u_id": user1["auth_user_id"]}) 
+    response3 = requests.get(f"{src.config.url}/user/profile/v1", params = {"token": user1["token"], "u_id": user1["auth_user_id"]}) 
 
     return_val = response3.json()
     assert return_val == {'user': {
@@ -26,49 +24,49 @@ def test_correct_output():
     }}
 
 def test_invalid_handle_length1():  
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathalmanjotbhathalmanjotbhathalmanjotbhathal"})
+    response2 = requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathalmanjotbhathalmanjotbhathalmanjotbhathal"})
     assert response2.status_code == 400
 
 def test_invalid_handle_length2():  
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "m"})
+    response2 = requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "m"})
     assert response2.status_code == 400   
 
 def test_invalid_handle_character_type():    
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjot!@#&*()-+"})
+    response2 = requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjot!@#&*()-+"})
     assert response2.status_code == 400  
 
 def test_handle_already_in_use():    
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "manjotbhathal@gmail.com", "password": "ManjotBhathal4", "name_first": "Manjot", "name_last": "Bhathal"})
+    response2 = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "manjotbhathal@gmail.com", "password": "ManjotBhathal4", "name_first": "Manjot", "name_last": "Bhathal"})
     assert response2.status_code == 200 
 
-    response3 = requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathal"})
+    response3 = requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": user1["token"], "handle_str": "manjotbhathal"})
     assert response3.status_code == 400 
 
 def test_invalid_token():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     assert response.status_code == 200
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/sethandle/v1", json = {"token": "invalid token", "handle_str": "manjotbhathal"})
+    response2 = requests.put(f"{src.config.url}/user/profile/sethandle/v1", json = {"token": "invalid token", "handle_str": "manjotbhathal"})
     assert response2.status_code == 403
--- tests/user_profile_setname_v1_http_test.py
+++ tests/user_profile_setname_v1_http_test.py
@@ -1,20 +1,17 @@
 import requests
 import pytest
-
-BASE_ADDRESS = 'http://127.0.0.1'
-BASE_PORT = 8080
-BASE_URL = f"{BASE_ADDRESS}:{BASE_PORT}"
+import src.config
 
 def test_correct_output():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user = response.json()
     
-    response2 =  requests.put(f"{BASE_URL}/user/profile/setname/v1", json= {"token": user['token'], "name_first": "Monta", "name_last": "Jree"})  
+    response2 =  requests.put(f"{src.config.url}/user/profile/setname/v1", json= {"token": user['token'], "name_first": "Monta", "name_last": "Jree"})  
     assert response2.status_code == 200
 
-    response3 = requests.get(f"{BASE_URL}/user/profile/v1", params = {"token": user['token'], "u_id": user['auth_user_id']}) 
+    response3 = requests.get(f"{src.config.url}/user/profile/v1", params = {"token": user['token'], "u_id": user['auth_user_id']}) 
 
     return_val = response3.json()
     assert return_val == {'user': {
@@ -26,45 +23,45 @@ def test_correct_output():
     }}
 
 def test_invalid_first_name1():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Montamontamontamontamontamontamontamontamontamontamonta", "name_last": "Jree"})
+    response2 = requests.put(f"{src.config.url}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Montamontamontamontamontamontamontamontamontamontamonta", "name_last": "Jree"})
     assert response2.status_code == 400   
 
 def test_invalid_first_name2():  
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "", "name_last": "Jree"})
+    response2 = requests.put(f"{src.config.url}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "", "name_last": "Jree"})
     assert response2.status_code == 400  
 
 def test_invalid_last_name1():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Monta", "name_last": "Jreemontamontamontamontamontamontamontamontamontamontamontamonta"})
+    response2 = requests.put(f"{src.config.url}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Monta", "name_last": "Jreemontamontamontamontamontamontamontamontamontamontamontamonta"})
     assert response2.status_code == 400  
 
 def test_invalid_last_name2():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     user1 = response.json()
 
-    response2 = requests.put(f"{BASE_URL}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Monta", "name_last": ""})
+    response2 = requests.put(f"{src.config.url}/user/profile/setname/v1", json = {"token": user1["token"], "name_first": "Monta", "name_last": ""})
     assert response2.status_code == 400 
 
 def test_invalid_token():
-    requests.delete(f"{BASE_URL}/clear/v1")
+    requests.delete(f"{src.config.url}/clear/v1")
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
+    response = requests.post(f"{src.config.url}/auth/register/v2", json = {"email": "jerrylin@gmail.com", "password": "JerrYlin4", "name_first": "Jerry", "name_last": "Lin"})
     assert response.status_code == 200
-    response2 = requests.put(f"{BASE_URL}/user/profile/setname/v1", json = {"token": -1, "name_first": "Monta", "name_last": "Jree"})
+    response2 = requests.put(f"{src.config.url}/user/profile/setname/v1", json = {"token": -1, "name_first": "Monta", "name_last": "Jree"})
     assert response2.status_code == 403   
