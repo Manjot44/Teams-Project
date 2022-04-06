d6232a818799b3e37b043433beaf725f1edb73f6
Jerry Lin <z5363361@ad.unsw.edu.au>
2022-04-06 22:53:21 +1000

    hopefully increase coverage through the extar tests

--- tests/message_edit_test.py
+++ tests/message_edit_test.py
@@ -111,3 +111,63 @@ def test_invalid_user_id(register_three_users):
 
     response = requests.put(f"{url}/message/edit/v1", json = {"token": 'invalid token', "message_id": message_id, "message": "Hello Iqti"})
     assert response.status_code == 403
+
+# Test if message edit can edit a dm message as it should
+def test_edit_dm(register_three_users):
+    response = requests.post(f"{url}/dm/create/v1", json = {"token": register_three_users["token"][0], "u_ids": [register_three_users["id"][0], register_three_users["id"][1]]})
+    assert response.status_code == 200
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    response = requests.post(f"{url}/message/senddm/v1", json = {"token": register_three_users["token"][0], "dm_id": dm_id, "message": "Hello Sanjam"})
+    assert response.status_code == 200
+    response_data = response.json()
+    message_id = response_data["message_id"]
+
+    response = requests.put(f"{url}/message/edit/v1", json = {"token": register_three_users["token"][0], "message_id": message_id, "message": "Hello Rahman Noodles"})
+    assert response.status_code == 200
+
+# Test if an empty dm is input as one of the arguments - should delete dm
+def test_empty_dm(register_three_users):
+    response = requests.post(f"{url}/dm/create/v1", json = {"token": register_three_users["token"][0], "u_ids": [register_three_users["id"][0], register_three_users["id"][1]]})
+    assert response.status_code == 200
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    response = requests.post(f"{url}/message/senddm/v1", json = {"token": register_three_users["token"][0], "dm_id": dm_id, "message": "Hello Sanjam"})
+    assert response.status_code == 200
+    response_data = response.json()
+    message_id = response_data["message_id"]
+
+    response = requests.put(f"{url}/message/edit/v1", json = {"token": register_three_users["token"][0], "message_id": message_id, "message": None})
+    assert response.status_code == 200
+
+# Test - User did not send the message, but is an owner member, should go through (for DMs)
+def test_dm_not_from_user(register_three_users):
+    response = requests.post(f"{url}/dm/create/v1", json = {"token": register_three_users["token"][0], "u_ids": [register_three_users["id"][0], register_three_users["id"][1]]})
+    assert response.status_code == 200
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    response = requests.post(f"{url}/message/senddm/v1", json = {"token": register_three_users["token"][1], "dm_id": dm_id, "message": "Hello Sanjam"})
+    assert response.status_code == 200
+    response_data = response.json()
+    message_id = response_data["message_id"]
+
+    response = requests.put(f"{url}/message/edit/v1", json = {"token": register_three_users["token"][0], "message_id": message_id, "message": "doctor sureshkumar I got a heart attack"})
+    assert response.status_code == 200
+
+# test - unauthorised person cant edit dm
+def test_dm_not_from_user(register_three_users):
+    response = requests.post(f"{url}/dm/create/v1", json = {"token": register_three_users["token"][0], "u_ids": [register_three_users["id"][0], register_three_users["id"][1]]})
+    assert response.status_code == 200
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    response = requests.post(f"{url}/message/senddm/v1", json = {"token": register_three_users["token"][0], "dm_id": dm_id, "message": "Hello Sanjam"})
+    assert response.status_code == 200
+    response_data = response.json()
+    message_id = response_data["message_id"]
+
+    response = requests.put(f"{url}/message/edit/v1", json = {"token": register_three_users["token"][1], "message_id": message_id, "message": "doctor sureshkumar I got a heart attack"})
+    assert response.status_code == 403
