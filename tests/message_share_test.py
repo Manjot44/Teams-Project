a1aba9b26242fdd01112cd0f0d797f6a0797e291
Jerry Lin <z5363361@ad.unsw.edu.au>
2022-04-13 02:42:57 +1000

    added more tests to improve coverage

--- tests/message_share_test.py
+++ tests/message_share_test.py
@@ -19,9 +19,25 @@ def test_normal_channel_share(register_three_users):
 
     response = requests.post(f"{url}/message/share/v1", json = {"token": register_three_users["token"][0], "og_message_id": message_id, "message": "Wow cool message", "channel_id": channel_id2, "dm_id": -1})
     assert response.status_code == 200
-# def test_normal_dm_share(register_three_users):
 
-# def test_empty_string(register_three_users):
+def test_normal_dm_share(register_three_users):
+    response = requests.post(f"{url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
+    assert response.status_code == 200
+    response_data = response.json()
+    channel_id = response_data["channel_id"]
+
+    response = requests.post(f"{url}/message/send/v1", json = {"token": register_three_users["token"][0], "channel_id": channel_id, "message": "Hello Sanjam"})
+    assert response.status_code == 200
+    response_data = response.json()
+    message_id = response_data["message_id"]
+
+    response = requests.post(f"{url}/dm/create/v1", json = {"token": register_three_users["token"][0], "u_ids": [register_three_users["id"][0]]})
+    assert response.status_code == 200
+    response_data = response.json()
+    dm_id = response_data["dm_id"]
+
+    response = requests.post(f"{url}/message/share/v1", json = {"token": register_three_users["token"][0], "og_message_id": message_id, "message": "Wow cool message", "channel_id": -1, "dm_id": dm_id})
+    assert response.status_code == 200
 
 def test_invalid_token(register_three_users):
     response = requests.post(f"{url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
@@ -112,7 +128,6 @@ def test_not_in_channel(register_three_users):
     response = requests.post(f"{url}/message/share/v1", json = {"token": register_three_users["token"][1], "og_message_id": message_id, "message": "Wow cool message", "channel_id": channel_id, "dm_id": -1})
     assert response.status_code == 403
 
-# neither channel_id or dm_id are minus one
 def test_neither_minus_one(register_three_users):
     response = requests.post(f"{url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
@@ -127,7 +142,6 @@ def test_neither_minus_one(register_three_users):
     response = requests.post(f"{url}/message/share/v1", json = {"token": register_three_users["token"][0], "og_message_id": message_id, "message": "Wow cool message", "channel_id": channel_id, "dm_id": 2})
     assert response.status_code == 400
 
-# both channel_id and dm_id are -1
 def test_both_minus_one(register_three_users):
     response = requests.post(f"{url}/channels/create/v2", json = {"token": register_three_users["token"][0], "name": "channel_name", "is_public": True})
     assert response.status_code == 200
