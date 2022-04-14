dc940061880c5a54d0517684f390829ba0a10c5b
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-04-14 21:06:31 +1000

    Created standup file.

---
+++ src/standup.py
@@ -0,0 +1,12 @@
+import src.error_help
+import src.persistence
+from datetime import datetime
+
+def standup_start_v1(token, channel_id, length):
+    
+
+def standup_active_v1(token, channel_id):
+    pass
+
+def standup_send_v1(token, channel_id, message):
+    pass
