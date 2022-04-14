419d818ee1373ff6c8f8b41036417f9165b701f6
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2022-04-14 10:39:15 +1000

    Progress on standup start tests.

--- tests/message_standup_start_v1_test.py
+++ tests/message_standup_start_v1_test.py
@@ -1,13 +1,20 @@
 import requests
 from src.config import url
+import datetime
 
 # REGULAR SEASON TESTS
 def test_standup_start_standup_period_0_sec(register_three_users):
     pass
+    
 
 def test_standup_start_standup_period_5_sec(register_three_users):
-    pass
+    token = register_three_users['token'][0]
+    channel = create_channel(token, 'CHANNEL_NAME', True)
+    response = requests.post("{url}")
 
+def test_standup_start_standup_period_
+
+datetime.
 
 # PRE SEASON TESTS - INPUT ERR
 def test_standup_start_channelid_invalid():
@@ -25,4 +32,3 @@ def test_standup_start_channel_id_valid_user_not_member():
 
 def test_standup_start_token_invalid():
     pass
-    
