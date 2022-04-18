ba0e7d716f93d574b8c98b43425196ece183973b
Jerry Lin <z5363361@ad.unsw.edu.au>
2022-04-18 14:41:57 +1000

    fixed pylint

--- tests/admin_user_remove_test.py
+++ tests/admin_user_remove_test.py
@@ -6,7 +6,7 @@ def test_valid_remove(register_three_users, create_channel, send_message):
     channel_id = create_channel(register_three_users["token"][1], 'CHANNEL_NAME', True)
 
     response = requests.post(f"{url}/channel/invite/v2", json = {"token": register_three_users["token"][1], "channel_id": channel_id, "u_id": register_three_users["id"][0]})
-    message_id = send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
+    send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
 
     response = requests.post(f"{url}/admin/userpermission/change/v1", json = {"token" : register_three_users["token"][0], "u_id" : register_three_users["id"][1], "permission_id" : 1})
     assert response.status_code == 200
@@ -26,7 +26,7 @@ def test_valid_remove(register_three_users, create_channel, send_message):
 # auth_user who is trying to remove another user has an invalid token
 def test_invalid_id(register_three_users, create_channel, send_message):
     channel_id = create_channel(register_three_users["token"][1], 'CHANNEL_NAME', True)
-    message_id = send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
+    send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
 
     response = requests.delete(f"{url}/admin/user/remove/v1", json = {"token": 'incorrect token', "u_id": register_three_users["id"][1]})
 
@@ -35,7 +35,7 @@ def test_invalid_id(register_three_users, create_channel, send_message):
 # uid is invalid
 def test_invalid_uid(register_three_users, create_channel, send_message):
     channel_id = create_channel(register_three_users["token"][1], 'CHANNEL_NAME', True)
-    message_id = send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
+    send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
     response = requests.delete(f"{url}/admin/user/remove/v1", json = {"token": register_three_users["token"][0], "u_id": register_three_users["id"][1] + 50})
 
     assert response.status_code == 400
@@ -43,7 +43,7 @@ def test_invalid_uid(register_three_users, create_channel, send_message):
 # Single global owner tries to remove themselves from Seams
 def test_only_global_owner(register_three_users, create_channel, send_message):
     channel_id = create_channel(register_three_users["token"][1], 'CHANNEL_NAME', True)
-    message_id = send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
+    send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
 
     response = requests.delete(f"{url}/admin/user/remove/v1", json = {"token": register_three_users["token"][0], "u_id": register_three_users["id"][0]})
 
@@ -52,7 +52,7 @@ def test_only_global_owner(register_three_users, create_channel, send_message):
 # non global owner tries to remove other user 
 def test_non_global_owner_remove(register_three_users, create_channel, send_message):
     channel_id = create_channel(register_three_users["token"][1], 'CHANNEL_NAME', True)
-    message_id = send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
+    send_message(register_three_users["token"][1], channel_id, "Hello Sanjam")
 
     response = requests.delete(f"{url}/admin/user/remove/v1", json = {"token": register_three_users["token"][1], "u_id": register_three_users["id"][0]})
 
@@ -60,7 +60,7 @@ def test_non_global_owner_remove(register_three_users, create_channel, send_mess
 
 def test_dm_channel(register_three_users, create_dm, send_messagedm):
     dm_id = create_dm(register_three_users["token"][0], [register_three_users["id"][0], register_three_users["id"][1]])
-    message_id = send_messagedm(register_three_users["token"][1], dm_id, "Hello Sanjam")
+    send_messagedm(register_three_users["token"][1], dm_id, "Hello Sanjam")
 
     response = requests.post(f"{url}/admin/userpermission/change/v1", json = {"token" : register_three_users["token"][0], "u_id" : register_three_users["id"][1], "permission_id" : 1})
     assert response.status_code == 200
